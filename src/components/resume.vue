ddf5fe409228483641ab2ad862de3df45b02a66d
firemonny <firemonny@hotmail.com>
2018-10-23 13:22:44 -0600

    first commit

---
+++ src/components/resume.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>
+            {{name}}
+        </p>
+        <button class="btn btn-primary"> Test for bootstrap</button>
+    </div>
+    
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                name: "Nung-Shun Chou"
+            }
+        }
+    }
+</script>
